59fb75cbdb94f3b761ec940c6bd087557228260a
Abhishek Chaudhary <acechd97@gmail.com>
2019-06-18 21:38:33 +0530

    Completed order history service

--- src/main/java/io/github/abhishekchd/restaurantbackend/services/OrderServiceImpl.java
+++ src/main/java/io/github/abhishekchd/restaurantbackend/services/OrderServiceImpl.java
@@ -4,14 +4,30 @@ import io.github.abhishekchd.restaurantbackend.dto.Order;
 import io.github.abhishekchd.restaurantbackend.dto.Status;
 import io.github.abhishekchd.restaurantbackend.exchanges.GetOrdersListResponse;
 import io.github.abhishekchd.restaurantbackend.exchanges.ModifiedOrderResponse;
+import io.github.abhishekchd.restaurantbackend.repositoryservices.OrderRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired OrderRepositoryService orderRepositoryService;
+
     @Override public GetOrdersListResponse getOrderHistory(String restaurantId) {
-        return new GetOrdersListResponse(new ArrayList<>());
+        List<Order> orders = orderRepositoryService.getAllOrders(restaurantId);
+        ArrayList<Order> previousOrders = new ArrayList<>();
+        for (Order order : orders) {
+            switch (order.getStatus()) {
+                case REJECTED:
+                case CANCELLED:
+                case COMPLETED:
+                    previousOrders.add(order);
+            }
+        }
+        return new GetOrdersListResponse(previousOrders);
     }
 
     @Override public GetOrdersListResponse getActiveOrders(String restaurantId) {
