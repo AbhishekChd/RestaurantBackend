831301b9e401ed4db75aa4d4f2f28256d9e4aa90
Abhishek Chaudhary <acechd97@gmail.com>
2019-06-18 11:58:58 +0530

    Initial Commit

---
+++ src/test/java/io/github/abhishekchd/restaurantbackend/RestaurantBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.abhishekchd.restaurantbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
