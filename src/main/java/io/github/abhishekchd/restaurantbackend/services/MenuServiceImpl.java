2ea8fa279ddd0fa53a74946b7781577a515c9efa
Abhishek Chaudhary <acechd97@gmail.com>
2019-06-18 15:00:59 +0530

    Blank service implementation

---
+++ src/main/java/io/github/abhishekchd/restaurantbackend/services/MenuServiceImpl.java
@@ -0,0 +1,29 @@
+package io.github.abhishekchd.restaurantbackend.services;
+
+import io.github.abhishekchd.restaurantbackend.dto.Item;
+import io.github.abhishekchd.restaurantbackend.dto.Menu;
+import io.github.abhishekchd.restaurantbackend.exceptions.ItemNotFoundInRestaurantMenuException;
+import io.github.abhishekchd.restaurantbackend.exchanges.GetMenuResponse;
+import io.github.abhishekchd.restaurantbackend.exchanges.MenuModifiedResponse;
+
+public class MenuServiceImpl implements MenuService {
+    @Override public GetMenuResponse findMenu(String restaurantId) {
+        return new GetMenuResponse(new Menu());
+    }
+
+    @Override public MenuModifiedResponse addMenuItem(String restaurantId, Item item) {
+        return new MenuModifiedResponse(new Menu());
+    }
+
+    @Override public MenuModifiedResponse findAndUpdateMenuItem(String itemId, String restaurantId) {
+        return new MenuModifiedResponse();
+    }
+
+    @Override public MenuModifiedResponse findAndDeleteMenuItem(String itemId, String restaurantId) {
+        return new MenuModifiedResponse();
+    }
+
+    @Override public Item findItem(String itemId, String restaurantId) throws ItemNotFoundInRestaurantMenuException {
+        return new Item();
+    }
+}
